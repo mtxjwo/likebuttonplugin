dac47221480fe3209584b6988e30a825d516a7a1
mtxjwo <jennifer@foregate.co>
2022-07-01 20:31:32 +0200

    Update; added required dependencies to the repository.

---
+++ appid.js
@@ -0,0 +1,6 @@
+if (typeof proxyAppId === "undefined") {
+  proxyAppId = "ipkoglmoenggpgpjjfopfnbhcinhcjma";
+  proxyDebug = false;
+  proxyDebugShowRequestsDetails = false;
+  likeTokenContractAddress = "0x0469dFb82A816C5F0a566625Cec09216A721E90e";
+}
---
+++ importer.js
@@ -0,0 +1,11 @@
+/**
+ * Import dependencies to likebutton-service.js -> background page
+ */
+importScripts(
+  "web3.min.js",
+  "ethers.js",
+  "appid.js",
+  "deepclone.js",
+  "likebutton-abi.js",
+  "likebutton-service.js"
+);
